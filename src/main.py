fc8c413382eec0c216181e48733523635793e592
Clinton Fray <clfray@cisco.com>
2023-09-16 21:28:47 +0200

    chore: make the script continious

--- src/main.py
+++ src/main.py
@@ -1,123 +1,123 @@
-import requests
 from requests.auth import HTTPBasicAuth
+import time
+import requests
+import urllib3
 import yaml
-import logging
-from urllib3.exceptions import InsecureRequestWarning
 import yamale
-from yamale import YamaleError
+import logging
 
 
-# Suppress the warnings from urllib3
-requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+urllib3.disable_warnings()
 
-logging.basicConfig(format=' %(asctime)s - %(levelname)s - %(message)s',
-                    datefmt='%m/%d/%Y %I:%M:%S %p', filename='cpnr_app.log', level=logging.INFO)
 
-def read_config():
+def main():
 
-    # YAML Schema Validation
+    global stats_hostname
+    global stats_port
+    global stats_username
+    global stats_password
+    global url_timeout
+
+    try:
+        # Get and Assign Configuration
+        stats_config = load_app_config()
+
+        stats_hostname = stats_config['stats']['endpoint']['hostname']
+        stats_port = stats_config['stats']['endpoint']['port']
+        stats_username = stats_config['stats']['auth']['user']
+        stats_password = stats_config['stats']['auth']['pass']
+        url_timeout = stats_config['defaultConfig']['api_timeout']
+
+        while True:
+            results = api_stats_connector(hostname=stats_hostname, port=stats_port,
+                                          username=stats_username, password=stats_password, verify=False)
+
+            for metric in stats_config.get('metrics'):
+
+                metric_name = metric['name']
+                metric_alias = metric['alias']
+
+                aggregation_type = metric['aggregationType']
+                time_rollup_type = metric['timeRollUpType']
+
+                try:
+
+                    if metric.get(f'old_{metric_name}') is None:
+                        metric[f'old_{metric_name}'] = int(
+                            results[metric_name])
+                    else:
+                        new_metric_value = int(results[metric_name])
+                        metric_value = new_metric_value - \
+                            metric[f'old_{metric_name}']
+
+                        try:
+                            print(
+                                f"name=Custom Metrics|DNSCachingServer|{metric_alias},aggregator={aggregation_type},time-rollup={time_rollup_type},value={metric_value}")
+
+                        except Exception as error:
+                            print(
+                                f"Error occurred: The value is not a 64bit Integer. Metric Name: {metric_name}, Metric Value: {metric_value}")
+                except Exception as error:
+                    print(
+                        f'Exception: Unable to retrieve the Metric: {metric_name}')
+
+            print('----------------------------')
+            time.sleep(30)
+
+    except Exception as e:
+        print(f'Error: {e}')
+
+
+def api_stats_connector(hostname=None, port=None, username=None, password=None, request_timeout=10, verify=True):
+
+    if (username is not None or password is not None):
+        basic_auth = HTTPBasicAuth(username, password)
+    else:
+        basic_auth = None
+
+    try:
+
+        headers = {'Accept': 'application/json'}
+        url = f'https://{hostname}:{port}/web-services/rest/stats/DNSCachingServer'
+
+        response = requests.get(
+            url, headers=headers, auth=basic_auth, verify=verify, timeout=request_timeout)
+
+        response.raise_for_status()
+
+    except Exception as e:
+        raise Exception(f'API Connector Exception: {e}')
+
+    return response.json()
+
+
+def load_app_config():
     try:
         file_schema = yamale.make_schema('./schema.yaml')
         file_config = yamale.make_data('./config.yaml')
 
         yamale.validate(file_schema, file_config)
-        
+
         logging.info('Validation success! 👍')
 
-    except YamaleError as e:
+    except yamale.YamaleError as e:
         print('Validation failed!')
         for result in e.results:
-            print('------------')
-            print(f"Error validating data {result.data}")
-            logging.error('------------')
-            logging.error(f"Error validating data {result.data}")
-            for error in result.errors:
-                print(f'\t{error}')
-                logging.error(f'\t{error}')
-        exit(1)
+            raise Exception(f"Error validating data {result.data}")
     except:
-        logging.error('Exception: We could not validate the config.yaml file!')
-        exit(1)
-
-    # Set Global Variables
-    global config
-    global api_user
-    global api_pass
-    global api_hostname
-    global api_port
-    global request_timeout
+        raise Exception(
+            'Exception: We could not validate the config.yaml file!')
 
     try:
         file_config_import = open('./config.yaml', 'r')
         config = yaml.safe_load(file_config_import)
         file_config_import.close()
     except Exception as e:
-        logging.error(
+        raise Exception(
             f'Exception: Unable to import the config.yaml file!, {e}')
-        exit(1)
 
-    stats = config.get('stats')
-
-    api_hostname = stats['endpoint']['hostname']
-    api_port = stats['endpoint']['port']
-
-    api_user = stats['auth']['user']
-    api_pass = stats['auth']['pass']
-
-    request_timeout = 10
+    return config
 
 
-def get_cpnr_stats():
-    try:
-        basic_auth = HTTPBasicAuth(api_user, api_pass)
-
-        headers = {'Accept': 'application/json'}
-        url = f'https://{api_hostname}:{api_port}/web-services/rest/stats/DNSCachingServer'
-
-        response = requests.get(
-            url, headers=headers, auth=basic_auth, verify=False, timeout=request_timeout)
-        response.raise_for_status()
-        results = response.json()
-
-        metric_list = config.get('metrics')
-
-        for m in metric_list:
-
-            try:
-                metric_name = m['name']
-                metric_alias = m['alias']
-
-                aggregation_type = m['aggregationType']
-                time_rollup_type = m['timeRollUpType']
-
-                try:
-                    metric_value = int(results[metric_name])
-                    try:
-                        print(
-                            f"name=Custom Metrics|DNSCachingServer|{metric_alias},aggregator={aggregation_type},time-rollup={time_rollup_type},value={metric_value}")
-                        logging.info(
-                            f"name=Custom Metrics|DNSCachingServer|{metric_alias},aggregator={aggregation_type},time-rollup={time_rollup_type},value={metric_value}")
-
-                    except Exception as error:
-                        logging.error('__________')
-                        print(error)
-                        logging.error(
-                            f"Error occurred: The value is not a 64bit Integer. Metric Name: {metric_name}, Metric Value: {metric_value}")
-                except Exception as error:
-                    logging.error(
-                        f'Exception: Unable to retrieve the Metric: {metric_name}')
-                    logging.error('__________')
-
-            except Exception as error:
-                logging.error(
-                    f'The metric name was not specified. Please check the config file.')
-        return
-    except Exception as error_message:
-        logging.error('Error occurred:' + str(error_message))
-        logging.error('Program Exit')
-        exit(1)
-
-
-if __name__ == "__main__":
-    read_config()
-    get_cpnr_stats()
+if __name__ == '__main__':
+    main()
