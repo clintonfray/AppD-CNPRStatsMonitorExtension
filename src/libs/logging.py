f100a24067d93c0ca7198ec2a192dfa3fdaefd48
Clinton Fray <clfray@cisco.com>
2023-09-19 14:50:32 +0200

    chore: removed the file size limiter

--- src/libs/logging.py
+++ src/libs/logging.py
@@ -8,11 +8,12 @@ logger = logging.getLogger('simple_logger')
 logger.setLevel(logging.DEBUG)
 
 # create rotating file handler and set level to debug
-handler = RotatingFileHandler('cnpr_app.log', maxBytes=2000, backupCount=2)
+handler = RotatingFileHandler('cnpr_app.log', backupCount=2)
 handler.setLevel(logging.DEBUG)
 
 # create formatter
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # add formatter to rotating file handler
 handler.setFormatter(formatter)
